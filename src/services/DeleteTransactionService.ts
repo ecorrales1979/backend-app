d0396ad03d683a31c5f47e3ea112db3cac09caa9
Eduardo Ernesto Corrales Reyna <eduardo.reyna@syonet.com>
2020-04-26 14:03:44 -0300

    Desafio concluido

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<boolean | AppError> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      return new AppError('Transaction does not exists');
+    }
+
+    await transactionRepository.remove(transaction);
+
+    return true;
   }
 }
 
